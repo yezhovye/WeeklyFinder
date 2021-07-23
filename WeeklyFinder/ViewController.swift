2677b6bb88941b367d58600ba8ae0fe6e0fe3228
Sawwaty <yezhovye@mac.com>
2021-07-23 23:24:54 +0300

    UI elements logic is added, localisation for result

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -18,6 +18,31 @@ class ViewController: UIViewController {
     
     
     @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dayTextField.text, let month = monthTextField.text, let year = yearTextField.text  else {
+            return }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekday = weekday.capitalized
+        
+        resultLabel.text = capitalizedWeekday
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
 
